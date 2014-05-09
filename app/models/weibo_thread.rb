58ada680325ca0da12f37a0de8d0c6431cc97f6e
fermin <fermin.su@xingxinghuo.com>
2014-05-09 17:18:37 +0800

    weibo thread

---
+++ app/models/weibo_thread.rb
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+class WeiboThread < ActiveRecord::Base
+  establish_connection YAML::load(File.open('config/database.yml'))['mvpdatabase']
+  self.table_name = "weibo_threads"
+end
